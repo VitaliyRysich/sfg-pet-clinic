0889c97507b066edfc8231878580bfa07fa52285
vitaliyRysich <vitaliy.rysich@gmail.com>
2020-03-21 17:14:59 +0100

    Added PetTypeService. Closes #29.

---
+++ pet-clinic-data/src/main/java/rysich/springframework/sfgpetclinic/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package rysich.springframework.sfgpetclinic.services;
+
+import rysich.springframework.sfgpetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
---
+++ pet-clinic-data/src/main/java/rysich/springframework/sfgpetclinic/services/map/PetTypeMapService.java
@@ -0,0 +1,33 @@
+package rysich.springframework.sfgpetclinic.services.map;
+
+import rysich.springframework.sfgpetclinic.model.PetType;
+import rysich.springframework.sfgpetclinic.services.PetTypeService;
+
+import java.util.Set;
+
+public class PetTypeMapService extends AbstractMapService<PetType, Long> implements PetTypeService {
+    @Override
+    public Set<PetType> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(PetType t) {
+        super.delete(t);
+    }
+
+    @Override
+    public PetType save(PetType t) {
+        return super.save(t);
+    }
+
+    @Override
+    public PetType findById(Long id) {
+        return super.findById(id);
+    }
+}
