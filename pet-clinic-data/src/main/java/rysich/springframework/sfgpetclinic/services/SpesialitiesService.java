c218e6a6297e6158b03648dc1b756aff94abb528
vitaliyRysich <vitaliy.rysich@gmail.com>
2020-03-21 18:45:56 +0100

    Added Specialities Map Impl. Closes #31.

---
+++ pet-clinic-data/src/main/java/rysich/springframework/sfgpetclinic/services/SpesialitiesService.java
@@ -0,0 +1,6 @@
+package rysich.springframework.sfgpetclinic.services;
+
+import rysich.springframework.sfgpetclinic.model.Speciality;
+
+public interface SpesialitiesService extends CrudService<Speciality, Long> {
+}
---
+++ pet-clinic-data/src/main/java/rysich/springframework/sfgpetclinic/services/map/SpecialityMapService.java
@@ -0,0 +1,35 @@
+package rysich.springframework.sfgpetclinic.services.map;
+
+import org.springframework.stereotype.Service;
+import rysich.springframework.sfgpetclinic.model.Speciality;
+import rysich.springframework.sfgpetclinic.services.SpesialitiesService;
+
+import java.util.Set;
+
+@Service
+public class SpecialityMapService extends AbstractMapService<Speciality, Long> implements SpesialitiesService {
+    @Override
+    public Set<Speciality> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Speciality speciality) {
+        super.delete(speciality);
+    }
+
+    @Override
+    public Speciality findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Speciality save(Speciality speciality) {
+        return super.save(speciality);
+    }
+}
